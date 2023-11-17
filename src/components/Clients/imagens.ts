917bcbf46817a41c52fd2486fb188f4814bc5618
renanpolese <renanpolese11@gmail.com>
2023-11-16 22:10:05 -0300

    cards bug fixes

--- src/components/Clients/imagens.ts
+++ src/components/Clients/imagens.ts
@@ -13,31 +13,19 @@ export const imagens: ClienteModel[] = [
     src: 'https://res.cloudinary.com/somasys2021/image/upload/v1638186078/site/logo_MARIN_500x500_fb_uqyauv.png',
     alt: 'Supermercado Marin',
   }),
-  new ClienteModel(4, 'Bertuol', {
-    src: 'https://res.cloudinary.com/somasys2021/image/upload/v1637954982/site/logotipo_bertuol_500x500_fb_bai55p.png',
-    alt: 'Supermercado Bertuol',
-  }),
-  new ClienteModel(5, 'Sucesso', {
-    src: 'https://res.cloudinary.com/somasys2021/image/upload/v1637954983/site/Sucesso_das_Carnes_500x500_l9wyj2.png',
-    alt: 'Supermercado Sucesso',
-  }),
-  new ClienteModel(6, 'Janh', {
+  new ClienteModel(4, 'Janh', {
     src: 'https://res.cloudinary.com/somasys2021/image/upload/v1637954982/site/Logo_jahn_x5ibxa.png',
     alt: 'Supermercado Jahn',
   }),
-  new ClienteModel(7, 'Trez', {
+  new ClienteModel(5, 'Trez', {
     src: 'https://res.cloudinary.com/somasys2021/image/upload/v1637954982/site/LOGOMARCA_trez_500x500_glhbmm.png',
     alt: 'Supermercado Trez',
   }),
-  new ClienteModel(8, 'Maccari', {
+  new ClienteModel(6, 'Maccari', {
     src: 'https://res.cloudinary.com/somasys2021/image/upload/v1637954982/site/maccari_500x500_xchdkf.png',
     alt: 'Supermercado Maccari',
   }),
-  new ClienteModel(9, 'Milenium', {
-    src: 'https://res.cloudinary.com/somasys2021/image/upload/v1637954982/site/milenium_hh1kj1.png',
-    alt: 'Supermercado Milenium',
-  }),
-  new ClienteModel(10, 'Central', {
+  new ClienteModel(7, 'Central', {
     src: 'https://res.cloudinary.com/somasys2021/image/upload/v1637954982/site/logo_central_nova_xxqnry.png',
     alt: 'Supermercado Central',
   }),
--- src/components/Services/Card/index.jsx
+++ src/components/Services/Card/index.jsx
@@ -1,51 +1,48 @@
 import Image from 'next/image';
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
-const Card = ({ rotation = 180, timing = 8000, children }) => {
+const Card = ({ timing = 1500, children }) => {
     const [isPassed, setIsPassed] = useState(false)
 
     const inner = {
         transform: isPassed
-            ? `rotateY(${rotation}deg)`
+            ? 'translateY(-92%)'
             : '',
-        transition: `transform 0.8s`
+
+        transition: `transform 0.6s`, 
+        boxShadow: '1px 2px 9px #00080EE0',
     }
 
     useEffect(() => {
         if (!isPassed) {
             return;
         }
+    });
+
+    const trigger = () => {
+        setIsPassed(true);
+    };
+
+    const reset = () => {
         const timeoutId = window.setTimeout(() => {
             setIsPassed(false);
         }, timing);
         return () => {
             window.clearTimeout(timeoutId);
         };
-    }, [isPassed, timing]);
-
-    const trigger = () => {
-        setIsPassed(true);
-    };
-
-
-
-    // const [isMobile, setIsMobile] = useState(false)
-    const back = {
-        transform: 'rotateY(180deg)',
-        background: children.style.backgroundImage,
-        backgroundSize: children.style.backgroundSize
     }
 
     return (
-        <div className='flex-grow flex md:text-left text-center'>
+        <div className='flex-grow flex md:text-left text-center'> 
             <div className='card mb-8 mx-auto overflow-hidden rounded-lg'>
                 <div className='inner h-full text-center hidden lg:block'
-                    onMouseEnter={trigger}
+                    onMouseEnter={trigger} onMouseMove={trigger}
+                    onMouseLeave={reset}
                     style={inner}>
 
-                    {/* FRENTE */}
+                    {/* CARD PC */}
                     <div className='front bg-card-front absolute w-full h-full'>
-                        <Image
+                        <Image 
                             unoptimized={true}
                             src={children.image.src}
                             alt={children.image.alt}
@@ -56,19 +53,7 @@ const Card = ({ rotation = 180, timing = 8000, children }) => {
                         <h1 className='text-white text-xl title-font font-medium mb-3'>
                             {children.title}
                         </h1>
-                    </div>
-
-                    {/* VERSO */}
-                    <div
-                        className='back z-0 absolute w-full h-full'
-                        style={back}
-                    >
-                        <div className='px-4 py-2 bg-black h-full z-40 opacity-80'>
-                            <h1 className='text-white text-4xl title-font font-medium mb-3'>
-                                {children.title}
-                            </h1>
-                            <CardItem>{children.lista}</CardItem>
-                        </div>
+                        <CardItem>{children.lista}</CardItem>
                     </div>
                 </div>
 
@@ -95,7 +80,7 @@ const CardItem = ({ children }) => {
         <div className='leading-relaxed text-base text-white'>
             <ul className='list-none'>
                 {children.map((item, indice) => {
-                    return <li key={indice}>{item}</li>
+                     return <li key={indice}>{item}</li>
                 })}
             </ul>
         </div>
